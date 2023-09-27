b267bc55464bbd767d37d8b46be8408932e4cedd
Alumno <Alumno@LAB1504-27>
2023-09-26 22:01:24 -0500

    Agregar archivos de proyecto.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static string connectionString = "Data Source=LAB1504-27\\SQLEXPRESS;Initial Catalog=Neptuno3;User ID=hilari;Password=hilari123";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Cargar la lista de clientes al iniciar la ventana
+            ListarClientes();
+        }
+
+        private void ListarClientes()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand("ListarCliente", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    dataGridClientes.ItemsSource = dataTable.DefaultView;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al listar los clientes: " + ex.Message);
+            }
+        }
+
+        private void btnRegistrar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand("InsertarCliente", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    // Agregar parámetros al comando
+                    command.Parameters.AddWithValue("@idCliente", txtIdCliente.Text);
+                    command.Parameters.AddWithValue("@NombreCompañia", txtNombreCompañia.Text);
+                    command.Parameters.AddWithValue("@NombreContacto", txtNombreContacto.Text);
+                    command.Parameters.AddWithValue("@CargoContacto", txtCargoContacto.Text);
+                    // Agregar los demás parámetros
+
+                    connection.Open();
+                    int rowsAffected = command.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Cliente registrado correctamente");
+                        ListarClientes();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al registrar el cliente: " + ex.Message);
+            }
+        }
+    }
+}
+
